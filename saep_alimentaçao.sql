c493a70dc9f3f99f2528de995cb987674357cfbc
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-12-01 21:53:24 -0300

    dfgd

---
+++ saep_alimentaçao.sql
@@ -0,0 +1,17 @@
+
+insert into Professores(nome,email,senha) values ('Jose Carlos','jose@gmail.com','jose123')
+insert into Professores(nome,email,senha) values ('João Carlos','joao@gmail.com','joao123')
+insert into Professores(nome,email,senha) values ('Carlos Jose','carlos@gmail.com','carlos123')
+
+
+--select *from Professores
+insert into Turmas(Nome_turma,Professor_ID) values ('1 serie',1)
+insert into Turmas(Nome_turma,Professor_ID) values ('2 serie',1)
+insert into Turmas(Nome_turma,Professor_ID) values ('3 serie',2)
+
+
+
+insert into Atividade (Descricao_Atividade,Turma_ID)values('Matematica Adicao',1)
+insert into Atividade (Descricao_Atividade,Turma_ID)values('Matematica divisao',1)
+insert into Atividade (Descricao_Atividade,Turma_ID)values('Matematica subtraçao',2)
+insert into Atividade (Descricao_Atividade,Turma_ID)values('Portugues ortografia',3)
